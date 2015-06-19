1283bd37014e5f6aee296b5f3ef413f8bacfff4e
Daniel Uranga <duranga@dokkogroup.com.ar>
2015-06-19 18:56:14 -0300

    Cache images as files instead of using SharedObject

--- Source/com/sempaigames/gplayrest/ui/BmpDataCache.hx
+++ Source/com/sempaigames/gplayrest/ui/BmpDataCache.hx
@@ -2,13 +2,16 @@ package com.sempaigames.gplayrest.ui;
 
 import flash.display.BitmapData;
 import flash.geom.Rectangle;
-import flash.net.SharedObject;
+import flash.utils.ByteArray;
 import haxe.crypto.Md5;
+import sys.FileSystem;
+import sys.io.File;
 
 class BmpDataCache {
 
+	static var cacheDir = "data";
 	static var instance : BmpDataCache;
-	var so : SharedObject;
+	static var maxDaysOld = 10;
 
 	public static function getInstance() {
 		if (instance==null) {
@@ -19,26 +22,43 @@ class BmpDataCache {
 
 	function new() {
 		try {
-			so = SharedObject.getLocal("gplusweb_bmpcache");
+			if (!FileSystem.exists(cacheDir)) {
+				FileSystem.createDirectory(cacheDir);
+			}
 		} catch (d : Dynamic) {
-			trace("Catched: " + d);
-			//SharedObject.deleteAll("gplusweb_bmpcache");
+
+		}
+	}
+
+	function daysOld(path : String) : Int {
+		try {
+			var d = Date.now().getDay()-FileSystem.stat(path).mtime.getDay();
+			return d<0 ? -d : d;
+		} catch (d : Dynamic) {
+			return 9999;
 		}
 	}
 
 	public function get(name : String) : BitmapData {
 		try {
-			return Reflect.getProperty(so.data, Md5.encode(name));
-		} catch (d : Dynamic) {
-			return null;
+			var path = cacheDir + "/" + Md5.encode(name);
+			if (daysOld(path)<maxDaysOld) {
+				var arr = File.getBytes(path);
+				var bmp = BitmapData.loadFromBytes(ByteArray.fromBytes(arr));
+				return bmp;
+			} else {
+				return null;
+			}
+		} catch (error : Dynamic) {
+			trace("d: " + error);
 		}
+		return null;
 	}
 
 	public function set(name : String, bmpData : BitmapData) {
-		Reflect.setField(so.data, Md5.encode(name), bmpData);
-		#if cpp
-		so.flush();
-		#end
+		var path = cacheDir + "/" + Md5.encode(name);
+		var arr = bmpData.encode("png", 1);
+		File.saveBytes(path, arr);
 	}
 
 }
