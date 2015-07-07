b7a7a7ff5d35eeed094378c6c460c084a721e219
Daniel Uranga <duranga@dokkogroup.com.ar>
2015-07-07 12:24:56 -0300

    Prevent input events from reaching below Gplusweb UI.

--- Source/com/sempaigames/gplayrest/ui/UIManager.hx
+++ Source/com/sempaigames/gplayrest/ui/UIManager.hx
@@ -2,13 +2,17 @@ package com.sempaigames.gplayrest.ui;
 
 import com.sempaigames.gplayrest.GPlay;
 import extension.nativedialog.NativeDialog;
+import flash.display.DisplayObject;
 import flash.events.Event;
 import flash.events.KeyboardEvent;
 import flash.Lib;
+import flash.system.Capabilities;
 
 class UIManager {
 
 	static var instance : UIManager = null;
+	var maxScreenResolution : Float;
+	var pushedUIelements : Array<DisplayObject>;
 	var viewsStack : Array<UI>;
 
 	public static function getInstance() {
@@ -19,10 +23,37 @@ class UIManager {
 	}
 
 	function new() {
+		maxScreenResolution = Math.max(Capabilities.screenResolutionX, Capabilities.screenResolutionY);
+		pushedUIelements = [];
 		viewsStack = [];
+		Lib.current.stage.addEventListener(Event.RESIZE, onResize);
 		Lib.current.stage.addEventListener(KeyboardEvent.KEY_DOWN, onKeyDown, 1000);
 		Lib.current.stage.addEventListener(KeyboardEvent.KEY_UP, onKeyUp, 1000);
-		Lib.current.stage.addEventListener(Event.RESIZE, onResize);
+	}
+
+	function pushOriginalUIDown() {
+		for (i in 0...Lib.current.stage.numChildren) {
+			var c = Lib.current.stage.getChildAt(i);
+			if ( !(Std.is(c, UI) && Lambda.has(viewsStack, cast(c, UI))) ) {
+				c.y += maxScreenResolution;
+				pushedUIelements.push(c);
+			}
+		}
+	}
+
+	function restoreOriginalUI() {
+		for (c in pushedUIelements) {
+			c.y -= maxScreenResolution;
+		}
+		pushedUIelements = [];
+	}
+
+	function updateOriginalUIStatus() {
+		if (viewsStack.length>0 && pushedUIelements.length==0) {
+			pushOriginalUIDown();
+		} else if (viewsStack.length==0 && pushedUIelements.length>0) {
+			restoreOriginalUI();
+		}
 	}
 
 	function onKeyDown(e : KeyboardEvent) {
@@ -50,6 +81,7 @@ class UIManager {
 		viewsStack.push(v);
 		Lib.current.stage.addChild(v);
 		v.onResize(null);
+		updateOriginalUIStatus();
 	}
 
 	public function showAllLeaderboards(gplay : GPlay) {
@@ -57,6 +89,7 @@ class UIManager {
 		viewsStack.push(v);
 		Lib.current.stage.addChild(v);
 		v.onResize(null);
+		updateOriginalUIStatus();
 	}
 
 	public function showLeaderboard(gplay : GPlay, id : String) {
@@ -64,6 +97,7 @@ class UIManager {
 		viewsStack.push(v);
 		Lib.current.stage.addChild(v);
 		v.onResize(null);
+		updateOriginalUIStatus();
 	}
 
 	public function closeCurrentView() {
@@ -72,6 +106,7 @@ class UIManager {
 			Lib.current.stage.removeChild(v);
 			v.onClose();
 		}
+		updateOriginalUIStatus();
 	}
 
 	public function onNetworkError() {
